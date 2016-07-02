1e11b057d067919610a77e4132bfb83f06e38e1b
Tran Du <untilyou158@gmail.com>
2016-07-02 20:34:36 +0700

    update lib

---
+++ episode1/handlers/get_key.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dutn158/GoIn5Minutes/episode1/storage"
+)
+
+func GetKey(db storage.DB) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			http.Error(w, "missing key name in query string", http.StatusBadRequest)
+			return
+		}
+		val, err := db.Get(key)
+		if err == storage.ErrNotFound {
+			http.Error(w, err, http.StatusNotFound)
+			return
+		} else if err != nil {
+			http.Error(w, fmt.Sprintf("error getting value from database: %s", err), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(val)
+	})
+}
--- episode1/handlers/put_key.go
+++ episode1/handlers/put_key.go
@@ -1,13 +1,33 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
 
-func GetKey(db DB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	"github.com/dutn158/GoIn5Minutes/episode1/storage"
+)
+
+func PutKey(db storage.DB) http.Handler {
+	return http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
 		key := r.URL.Query().Get("key")
+
 		if key == "" {
-			http.Error(w, "missing key name in query string", http.StatusBadRequest)
+			http.Error(w, "missing key name in path", http.StatusBadRequest)
 			return
 		}
+
+		defer r.Body.Close()
+
+		if err != nil {
+			http.Error(w, "error reading PUT body", http.StatusBadRequest)
+			return
+		}
+
+		if err := db.Set(key, val); err != nil {
+			http.Error(w, "error setting value in DB", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
 	})
 }
