f7dcec97bc0b71b7549ce11ef6d6bbfa065eb01f
Tran Du <untilyou158@gmail.com>
2016-07-09 02:06:43 +0700

    add model

--- TheNewStack_Restful/main.go
+++ TheNewStack_Restful/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+// Todo struct of the Todo
+type Todo struct {
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
+}
+
+// Todos slices
+type Todos []Todo
 
 // Index handle "/" request
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -16,22 +28,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 // TodoIndex handle "/todos" request
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "TodoIndex!")
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
 }
 
 // TodoShow handle "/todos/{todoId]}" request
 func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	fmt.Println(vars)
 	todoID := vars["todoId"]
-	fmt.Fprintf(w, "Todo show: ", todoID)
+	fmt.Fprintf(w, "Todo show: %q", todoID)
 }
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/todos", TodoIndex)
-	http.HandleFunc("/todos/{todoId}", TodoShow)
-	error := http.ListenAndServe(":3001", router)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	error := http.ListenAndServe(":8080", router)
 	if error != nil {
 		log.Fatal(error)
 	}
