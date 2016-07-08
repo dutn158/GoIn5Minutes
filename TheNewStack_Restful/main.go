5522976ca821395490bacd789bb42c64a0352eb3
Tran Du <untilyou158@gmail.com>
2016-07-08 23:59:06 +0700

    restful with gorilla mux

--- TheNewStack_Restful/main.go
+++ TheNewStack_Restful/main.go
@@ -5,15 +5,33 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// Index handle "/" request
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome, %q", html.EscapeString(r.URL.Path))
+}
+
+// TodoIndex handle "/todos" request
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "TodoIndex!")
+}
+
+// TodoShow handle "/todos/{todoId]}" request
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoId"]
+	fmt.Fprintf(w, "Todo show: ", todoID)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	error := http.ListenAndServe(":8080", nil)
-
+	router := mux.NewRouter().StrictSlash(true)
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/todos", TodoIndex)
+	http.HandleFunc("/todos/{todoId}", TodoShow)
+	error := http.ListenAndServe(":3001", router)
 	if error != nil {
 		log.Fatal(error)
 	}
