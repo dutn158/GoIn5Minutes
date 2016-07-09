4296816e732d46c9bc8716e4f59377143ec18162
Tran Du <untilyou158@gmail.com>
2016-07-09 12:00:55 +0700

    add repo

--- TheNewStack_Restful/handlers.go
+++ TheNewStack_Restful/handlers.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"html"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -16,10 +18,8 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 // TodoIndex handle "/todos" request
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	err := json.NewEncoder(w).Encode(todos)
 
@@ -35,4 +35,31 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(vars)
 	todoID := vars["todoId"]
 	fmt.Fprintf(w, "Todo show: %q", todoID)
+}
+
+// TotoCreate... handle "/todos" request with POST
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.Header().Set("Content-Type", "application/json;chartset=UTF-8")
+		w.WriteHeader(422)
+		if error := json.NewEncoder(w).Encode(err); error != nil {
+			panic(error)
+		}
+	}
+
+	t := RepoCreateTodo(todo)
+	w.Header().Set("Content-Type", "application/json;chartset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
+
 }
---
+++ TheNewStack_Restful/repo.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var currentID int
+
+var todos Todos
+
+func init() {
+	RepoCreateTodo(Todo{Name: "Github.com"})
+	RepoCreateTodo(Todo{Name: "Google.com"})
+}
+
+// RepoCreateTodo create a todo
+func RepoCreateTodo(t Todo) Todo {
+	currentID++
+	t.Id = currentID
+	todos = append(todos, t)
+	return t
+}
+
+// RepoFindTodo find a todo with id
+func RepoFindTodo(id int) Todo {
+	for _, t := range todos {
+		if t.Id == id {
+			return t
+		}
+	}
+	return Todo{}
+}
+
+// RepoDestroyTodo delete a todo with id
+func RepoDestroyTodo(id int) error {
+	for i, t := range todos {
+		if t.Id == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find Todo with id of %d to delete", id)
+}
