6646ae26ad6d310eccd108b5fab6673bafce9eb9
Tran Du <untilyou158@gmail.com>
2016-07-09 11:26:21 +0700

    add logger

--- TheNewStack_Restful/routes.go
+++ TheNewStack_Restful/routes.go
@@ -1,45 +1,49 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gorilla/mux"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Route struct {
-  Name string
-  Method string
-  Pattern string
-  HanderFunc http.HandlerFunc
+	Name       string
+	Method     string
+	Pattern    string
+	HanderFunc http.HandlerFunc
 }
 
 type Routes []Route
 
-var routes = Routes {
-  Route {
-    "Index",
-    "GET",
-    "/",
-    Index,
-  },
-  Route {
-    "TodoIndex",
-    "GET",
-    "/todos",
-    TodoIndex,
-  },
-  Route {
-    "TodoShow",
-    "GET",
-    "/todos/{todoId}",
-    TodoShow,
-  }
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"TodoIndex",
+		"GET",
+		"/todos",
+		TodoIndex,
+	},
+	Route{
+		"TodoShow",
+		"GET",
+		"/todos/{todoId}",
+		TodoShow,
+	},
 }
 
 func NewRouter() *mux.Router {
-  router := mux.NewRouter().StrictSlash(true)
-  for _, route := range routes {
-    router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(route.HanderFunc)
-  }
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+		handler = route.HanderFunc
+		handler = Logger(handler, route.Name)
+		router.Methods(route.Method).Path(route.Pattern).Name(route.Name).Handler(handler)
+	}
 
-  return router;
+	return router
 }
