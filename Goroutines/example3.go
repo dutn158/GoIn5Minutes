f14220d44ee700477558a740f0cb4998dd206233
Tran Du <untilyou158@gmail.com>
2016-07-16 23:27:18 +0700

    channel selected

---
+++ Goroutines/example3.go
@@ -0,0 +1,42 @@
+/**
+	Go has a special statement called select that works like a switch but for channels:
+ */
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from c1"
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "from c2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case msg1 := <- c1:
+				fmt.Println("msg1:", msg1)
+			case msg2 := <- c2:
+				fmt.Println("msg2 ", msg2)
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+}
