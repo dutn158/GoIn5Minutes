600a21f58cf91a3c93ceaca7db1ea3a2876d9dab
Tran Du <untilyou158@gmail.com>
2016-07-17 19:01:25 +0700

    add gorilla API

---
+++ MasteringGoWebService/example2Gorilla.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+)
+
+type API struct {
+	Message string `json:"message"`
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	name := urlParams["user"]
+	HelloMessage := "Hello, " + name
+	message := API{
+		Message: HelloMessage,
+	}
+	output, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Something went wrong")
+	}
+
+	fmt.Fprintf(w, string(output))
+}
+
+func main() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/api/{user:[0-9]+}", Hello)
+	http.Handle("/", mux)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
